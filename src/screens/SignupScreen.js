a7f03f411130469664b1a836d73417d5ff1aabad
KiriZhu <kiribaty88@gmail.com>
2020-10-16 01:28:13 +0200

    added scrollview and keyboardavoidingview to fix bug were button was dissapering

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,14 +1,59 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  View,
+  KeyboardAvoidingView,
+  ScrollView,
+} from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
+import Spacer from '../components/Spacer';
 
 const SignupScreen = () => {
   return (
-    <View>
-      <Text></Text>
-    </View>
+    <KeyboardAvoidingView style={styles.flexGrow}>
+      <ScrollView style={styles.contentContainerStyle}>
+        <View style={styles.container}>
+          <Spacer>
+            <Text h3 style={styles.textCenter}>
+              Sign Up for Tracks
+            </Text>
+          </Spacer>
+          <Spacer />
+          <Spacer>
+            <Input label='Email' />
+          </Spacer>
+          <Spacer>
+            <Input label='Password' />
+          </Spacer>
+        </View>
+      </ScrollView>
+
+      <View style={styles.buttonWrapper}>
+        <Button title='Sign Up' />
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
 export default SignupScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  flexGrow: {
+    flexGrow: 1,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  textCenter: {
+    textAlign: 'center',
+  },
+  contentContainerStyle: {
+    paddingTop: 120,
+  },
+  buttonWrapper: {
+    marginBottom: 30,
+    marginHorizontal: 10,
+    marginTop: -20,
+  },
+});
